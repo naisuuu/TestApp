a3296deb17d242e2805aafaebbefe7661d7a48ec
Cyril Jacob Mosawan <48050707+naisuuu@users.noreply.github.com>
2020-03-31 19:21:11 +0100

    inserted toolbar with 'logout' no functionality yet

--- app/src/main/java/com/example/sos/UserActivity.java
+++ app/src/main/java/com/example/sos/UserActivity.java
@@ -1,10 +1,16 @@
 package com.example.sos;
 
 import android.content.Intent;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -12,12 +18,14 @@ public class UserActivity extends AppCompatActivity {
     Button btnLogOut;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private Toolbar mToolbar;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        btnLogOut = findViewById(R.id.btnLogOut);
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,8 +34,18 @@ public class UserActivity extends AppCompatActivity {
                 Intent I = new Intent(UserActivity.this, ActivityLogin.class);
                 startActivity(I);
 
+
             }
         });
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("TESTT");
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+       super.onCreateOptionsMenu(menu);
+       getMenuInflater().inflate(R.menu.main_menu, menu);
+       return true;
     }
 }
