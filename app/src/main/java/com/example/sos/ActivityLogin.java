c5d7c0d47b1dc6153456bbd72338c35b148879cc
Ian <ianf101202303@gmail.com>
2020-04-02 16:13:07 +0100

    Test

--- app/src/main/java/com/example/sos/ActivityLogin.java
+++ app/src/main/java/com/example/sos/ActivityLogin.java
@@ -16,7 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+///Hey///
 public class ActivityLogin extends AppCompatActivity {
     public EditText loginEmailId, logInpasswd;
     Button btnLogIn;
