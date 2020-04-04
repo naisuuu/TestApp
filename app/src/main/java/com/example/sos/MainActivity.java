c6b7d93d89cd6640fd3f67358270b2c216063179
Ian <ianf101202303@gmail.com>
2020-04-04 17:05:50 +0100

    added Users Activity ui components for firebase gradle Users Java class User single layout xml

--- app/src/main/java/com/example/sos/MainActivity.java
+++ app/src/main/java/com/example/sos/MainActivity.java
@@ -57,6 +57,15 @@ public class MainActivity extends AppCompatActivity {
             Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(settingsIntent);
         }
+
+        if(item.getItemId()==R.id.main_all_btn){
+
+            Intent settingsIntent = new Intent(MainActivity.this, UsersActivity.class);
+            startActivity(settingsIntent);
+        }
+
+
+
         return true;
     }
 }
--- app/src/main/java/com/example/sos/UsersActivity.java
+++ app/src/main/java/com/example/sos/UsersActivity.java
@@ -1,12 +1,19 @@
 package com.example.sos;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 
 
 public class UsersActivity extends AppCompatActivity {
@@ -26,12 +33,49 @@ public class UsersActivity extends AppCompatActivity {
         ///                 mUsersList = (RecyclerView) findViewById(R.id.users_list);
 
         mToolbar = (Toolbar) findViewById(R.id.users_appBar);
-        getSupportActionBar().setTitle("All Users");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setSupportActionBar(mToolbar);
 
+        getSupportActionBar().setTitle("All Users");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mUsersList = findViewById(R.id.users_list);
+        mUsersList = (RecyclerView) findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
 
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+    FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>() {
+        @Override
+        protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users model) {
+
+        }
+
+        @NonNull
+        @Override
+        public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+    }
+
+    }
+
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+
+    }
+
 }
