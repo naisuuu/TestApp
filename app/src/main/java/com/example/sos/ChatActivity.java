ec2b7e7f8e63ed3590488681654c1e49de8ae31e
Ian <ianf101202303@gmail.com>
2020-04-08 20:22:09 +0100

    Chat_custom_bar xml Activity_chat xml Chat Activity LapitChat

--- app/src/main/java/com/example/sos/ChatActivity.java
+++ app/src/main/java/com/example/sos/ChatActivity.java
@@ -1,16 +1,72 @@
 package com.example.sos;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class ChatActivity extends AppCompatActivity {
+
+    private String mChatUser;
+    private Toolbar mChatToolbar;
+    private DatabaseReference mRootRef;
+
+
+    private TextView mTitleView;
+    private TextView mLastSeenView;
+    private CircleImageView mProfileImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
+        mChatToolbar = (Toolbar) findViewById(R.id.chat_app_bar);
+        setSupportActionBar(mChatToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+
+        mChatUser = getIntent().getStringExtra("user_id");
+        String userName = getIntent().getStringExtra("user_id");
+
+        ///getSupportActionBar().setTitle(userName);
+
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View action_bar_view = inflater.inflate(R.layout.chat_custom_bar, null);
+        actionBar.setCustomView(action_bar_view);
+
+
+        // -----------------Custom Action Bar Items -----------------------------------------------------
+
+        mTitleView = (TextView)findViewById(R.id.custom_bar_title);
+        mLastSeenView = (TextView)findViewById(R.id.custom_bar_seen);
+        mProfileImage = (CircleImageView)findViewById(R.id.custom_bar_image);
+
+        mTitleView.setText(userName);
+
     }
-}
+
+    }
