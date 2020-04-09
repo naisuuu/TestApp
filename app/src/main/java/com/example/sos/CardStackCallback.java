907896f1a6e0f646cc8e6c55b3f36aeb4d9e851d
Cyril Jacob Mosawan <48050707+naisuuu@users.noreply.github.com>
2020-04-09 18:15:50 +0100

    merged message and swipe features (not working)
    Signed-off-by: Cyril Jacob Mosawan <48050707+naisuuu@users.noreply.github.com>

---
+++ app/src/main/java/com/example/sos/CardStackCallback.java
@@ -0,0 +1,35 @@
+package com.example.sos;
+
+import androidx.recyclerview.widget.DiffUtil;
+
+import java.util.List;
+
+public class CardStackCallback extends DiffUtil.Callback {
+
+    private List<ItemModel> old, fresh;
+
+    public CardStackCallback(List<ItemModel> old, List<ItemModel> fresh) {
+        this.old = old;
+        this.fresh = fresh;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return old.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return fresh.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return old.get(oldItemPosition).getImage() == fresh.get(newItemPosition).getImage();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return old.get(oldItemPosition) == fresh.get(newItemPosition);
+    }
+}
