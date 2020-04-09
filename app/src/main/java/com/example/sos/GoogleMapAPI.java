0014d46f28cb98abb68547a5b3aac85812cc5dd7
Lucasrv79 <Lucasrv79@gmail.com>
2020-04-09 20:46:32 +0100

    Merge branch 'master' of C:\Users\Administrador\Desktop\TestApp with conflicts.

---
+++ app/src/main/java/com/example/sos/GoogleMapAPI.java
@@ -0,0 +1,38 @@
+package com.example.sos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GoogleMapAPI extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_map_a_p_i);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapAPI = googleMap;
+
+        LatLng NCI = new LatLng(53.349155, -6.242805);
+        mapAPI.addMarker(new MarkerOptions().position(NCI).title("National College of Ireland"));
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(NCI));
+    }
+}
